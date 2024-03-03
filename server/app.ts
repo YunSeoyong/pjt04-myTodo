06901524c455180ec4e8096b063630b2bee16f47
YunSeoyong <seoy2515@gmail.com>
2024-03-04 00:04:36 +0900

    db기초, env삭제

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,60 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const bodyParser = require("body-parser");
+
+import express from "express";
+import mongoose from "mongoose";
+import { TodoModel, TodoDocument } from "./models/todo";
 
 const app = express();
+const PORT = 3000;
+const dbConnect = require('../config/dbConnect');
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, 'views'));
+dbConnect();
+app.use(express.json());
 
-app.use("/public", express.static(__dirname + '/public'));
+// Define routes for CRUD operations
+app.get("/todos", async (req, res) => {
+    try {
+        const todos = await TodoModel.find();
+        res.json(todos);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.post("/todos", async (req, res) => {
+    const todo = new TodoModel({
+        content: req.body.content,
+        date: new Date(),
+        isDone: false,
+    });
+
+    try {
+        const newTodo = await todo.save();
+        res.status(201).json(newTodo);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.put("/todos/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const updatedTodo = await TodoModel.findByIdAndUpdate(id, req.body, {
+            new: true,
+        });
+        res.json(updatedTodo);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await TodoModel.findByIdAndDelete(id);
+        res.json({ message: "Todo deleted" });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
