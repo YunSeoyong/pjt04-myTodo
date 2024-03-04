07a7bd97b979b89050e47eaf2a37f8b93c7ae5b2
YunSeoyong <seoy2515@gmail.com>
2024-03-04 12:08:00 +0900

    firebase 배포

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAt3JifzOZI9SloHv7f36XGJtBP9vQKQUA",
+  authDomain: "mytodotype.firebaseapp.com",
+  projectId: "mytodotype",
+  storageBucket: "mytodotype.appspot.com",
+  messagingSenderId: "670378519457",
+  appId: "1:670378519457:web:702d29f4d3f4fc5b64b9e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
